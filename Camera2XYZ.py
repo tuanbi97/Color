e61dbf95cf794cdba3a4f7a99684b403f6250902
tuanbi97 <tuanbi97@gmail.com>
2018-10-08 16:24:17 -0500

    XYZ to RGB

--- Camera2XYZ.py
+++ Camera2XYZ.py
@@ -1,9 +1,26 @@
 import rawpy as rp
+import numpy as np
+
+def XYZ2RGB(pixel, gamma = 2.2, illuminant = 'D50'):
+    if illuminant == 'D65':
+        M = [[3.2404542, -1.5371385, -0.4985314],
+            [-0.9692660, 1.8760108, 0.0415560],
+            [0.0556434, -0.2040259, 1.0572252]]
+    if illuminant == 'D50':
+        M = [[3.1338561, -1.6168667, -0.4906146],
+            [-0.9787684, 1.9161415, 0.0334540],
+            [0.0719453, -0.2289914, 1.4052427]]
+    rgb = np.array([M[0][0] * pixel[0] + M[0][1] * pixel[1] + M[0][2] * pixel[2],
+            M[1][0] * pixel[0] + M[1][1] * pixel[1] + M[1][2] * pixel[2],
+            M[2][0] * pixel[0] + M[2][1] * pixel[1] + M[2][2] * pixel[2]])
+    rgb = rgb ** (1.0/gamma)
+    rgb *= 255
+    return rgb.astype(int)
 
 def getXYZD65(im, roi):
     M = [[0.9555766, -0.0230393, 0.0631636],
-                [-0.0282895, 1.0099416, 0.0210077],
-                [0.0122982, -0.0204830, 1.3299098]]
+        [-0.0282895, 1.0099416, 0.0210077],
+        [0.0122982, -0.0204830, 1.3299098]]
     X = Y = Z = 0.0
     for i in range(roi[1], roi[3]):
         for j in range(roi[0], roi[2]):
@@ -18,9 +35,10 @@ def getXYZD65(im, roi):
             M[1][0] * X + M[1][1] * Y + M[1][2] * Z,
             M[2][0] * X + M[2][1] * Y + M[2][2] * Z]
 
-def rawprocess(fpath):
+def rawprocess(fpath, norm = False):
     with rp.imread(fpath) as raw:
         user_wb = [1.868109862326931, 1.0, 1.4500039644365266, 0]
+        #user_wb = raw.camera_whitebalance
         rgb = raw.postprocess(demosaic_algorithm=rp.DemosaicAlgorithm.LINEAR,
                               half_size=False,
                               four_color_rgb=False,
@@ -31,4 +49,7 @@ def rawprocess(fpath):
                               output_bps=8,
                               no_auto_bright=True,
                               gamma=(1, 1))
-        return rgb
+        if norm == False:
+            return rgb
+        else:
+            return rgb / 255.0
--- Train.py
+++ Train.py
@@ -1,10 +1,8 @@
 import numpy as np
-import Constant as cons
-import csv
 import json
-import rawpy as rp
 import Reflectance2XYZ as ref2xyz
 import Camera2XYZ as cam2xyz
+import matplotlib.pyplot as plt
 
 def distance(a, b):
     return np.sum((np.array(a) - np.array(b))**2)**0.5
@@ -23,7 +21,16 @@ data = ref2xyz.read_data('VOC 2014 Color Data.csv')
 print(len(data))
 for i in range(0, len(data)):
     data[i]['XYZ'] = ref2xyz.reflectance2XYZ(data[i]['ref'])
-    # print(data[i]['XYZ'])
+    #check visualization
+    # if data[i]['name'] == 'SCENTED VALENTINE':
+    #     xyz = data[i]['XYZ']
+    #     rgb = cam2xyz.XYZ2RGB(xyz, gamma=2.2, illuminant='D65')
+    #     print(rgb,' ', data[i]['RGB'])
+    #     sample = np.tile([int(x) for x in rgb], (300, 300, 1))
+    #     plt.imshow(sample)
+    #     plt.show()
+        #print(data[i]['XYZ'])
+    #print(data[i]['XYZ'])
     data[i]['mLAB'] = ref2xyz.XYZ2LAB(data[i]['XYZ'], 'd65')
 
 #Test
@@ -34,15 +41,37 @@ labelpath = 'E:/UIUC/Data_10_07_18/Android_label.json'
 with open(labelpath) as f:
     label = json.load(f)
 
-out = cam2xyz.rawprocess(path + fname)
+out = cam2xyz.rawprocess(path + fname, norm = True)
+fig_size = plt.rcParams["figure.figsize"]
+fig_size[0] = 12
+fig_size[1] = 9
+plt.rcParams["figure.figsize"] = fig_size
+# plt.imshow(out)
+# plt.show()
 acc = 0.0
-for i in range(0, 7):
+for i in range(3, 4):
+    print(i, ':')
     cc = label[fname][i]
     roi = cc['roi']
+    #visualize D50
+    sample = np.array(out[roi[1]:roi[3], roi[0]:roi[2]])
+    for j in range(0, 300):
+        for k in range(0, 300):
+            sample[j][k] = cam2xyz.XYZ2RGB(sample[j][k], gamma=2.2, illuminant='D50')
+    plt.imshow(sample.astype(int))
+    plt.show()
+
     XYZD65 = cam2xyz.getXYZD65(out, roi)
+
+    #visualize D65
+    for j in range(0, 300):
+        for k in range(0, 300):
+            sample[j][k] = cam2xyz.XYZ2RGB(XYZD65, gamma=2.2, illuminant='D65')
+    plt.imshow(sample.astype(int))
+    plt.show()
     print(XYZD65)
     ret = MatchColor(XYZD65)
-    print(ret)
+    print(ret, '  _  ', label[fname][i]['label'].upper())
     if ret == label[fname][i]['label']:
         acc += 1
 print(acc / 7)
