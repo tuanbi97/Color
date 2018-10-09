eb5ebbfc0e108daf8932ad0e8f4348f640eb69bc
tuanbi97 <tuanbi97@gmail.com>
2018-10-09 11:11:31 -0500

    XYZ to RGB

--- Train.py
+++ Train.py
@@ -22,13 +22,13 @@ print(len(data))
 for i in range(0, len(data)):
     data[i]['XYZ'] = ref2xyz.reflectance2XYZ(data[i]['ref'])
     #check visualization
-    # if data[i]['name'] == 'SCENTED VALENTINE':
-    #     xyz = data[i]['XYZ']
-    #     rgb = cam2xyz.XYZ2RGB(xyz, gamma=2.2, illuminant='D65')
-    #     print(rgb,' ', data[i]['RGB'])
-    #     sample = np.tile([int(x) for x in rgb], (300, 300, 1))
-    #     plt.imshow(sample)
-    #     plt.show()
+    if data[i]['name'] == 'RIVER ROUGE':
+        xyz = data[i]['XYZ']
+        rgb = cam2xyz.XYZ2RGB(xyz, gamma=2.2, illuminant='D65')
+        print(rgb,' ', data[i]['RGB'], ' ', data[i]['XYZ'])
+        # sample = np.tile([int(x) for x in rgb], (300, 300, 1))
+        # plt.imshow(sample)
+        # plt.show()
         #print(data[i]['XYZ'])
     #print(data[i]['XYZ'])
     data[i]['mLAB'] = ref2xyz.XYZ2LAB(data[i]['XYZ'], 'd65')
@@ -48,27 +48,38 @@ fig_size[1] = 9
 plt.rcParams["figure.figsize"] = fig_size
 # plt.imshow(out)
 # plt.show()
+wpd50 = [0.964220, 1.000000, 0.825210]
+best = 1000000000.0
+pos = (0, 0)
+for i in range(0, out.shape[0]):
+    for j in range(0, out.shape[1]):
+        if best > distance(out[i][j], wpd50):
+            best = distance(out[i][j], wpd50)
+            pos = (i, j)
+
+print (out[pos[0]][pos[1]])
 acc = 0.0
 for i in range(3, 4):
     print(i, ':')
     cc = label[fname][i]
     roi = cc['roi']
     #visualize D50
-    sample = np.array(out[roi[1]:roi[3], roi[0]:roi[2]])
-    for j in range(0, 300):
-        for k in range(0, 300):
-            sample[j][k] = cam2xyz.XYZ2RGB(sample[j][k], gamma=2.2, illuminant='D50')
-    plt.imshow(sample.astype(int))
-    plt.show()
+    # sample = np.array(out[roi[1]:roi[3], roi[0]:roi[2]], dtype=np.float)
+    # for j in range(0, 300):
+    #     for k in range(0, 300):
+    #         sample[j][k] = cam2xyz.XYZ2RGB(sample[j][k], gamma=1, illuminant='D50')
+    # plt.imshow(sample.astype(int))
+    # plt.show()
 
     XYZD65 = cam2xyz.getXYZD65(out, roi)
+    #print(cam2xyz.XYZ2RGB(XYZD65, gamma=1, illuminant='D65'))
 
     #visualize D65
-    for j in range(0, 300):
-        for k in range(0, 300):
-            sample[j][k] = cam2xyz.XYZ2RGB(XYZD65, gamma=2.2, illuminant='D65')
-    plt.imshow(sample.astype(int))
-    plt.show()
+    # for j in range(0, 300):
+    #     for k in range(0, 300):
+    #         sample[j][k] = cam2xyz.XYZ2RGB(XYZD65, gamma=1, illuminant='D65')
+    # plt.imshow(sample.astype(int))
+    # plt.show()
     print(XYZD65)
     ret = MatchColor(XYZD65)
     print(ret, '  _  ', label[fname][i]['label'].upper())
