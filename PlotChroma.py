649016421cb1290e8c65f11269d7c60cfa69a04b
tuanbi97 <tuanbi97@gmail.com>
2018-11-21 18:22:53 -0600

    Add chromatic plot

--- PlotChroma.py
+++ PlotChroma.py
@@ -2,6 +2,9 @@ import csv
 import colour
 import matplotlib.pyplot as plt
 import colour.plotting as cplot
+import Reflectance2XYZ as ref2xyz
+
+ppg_data = ref2xyz.read_data('VOC 2014 Color Data.csv')
 cplot.RGB_colourspaces_chromaticity_diagram_plot_CIE1931(['sRGB'], standalone=False)
 
 csv_result = './ResultIOS.csv'
@@ -14,10 +17,15 @@ with open(csv_result, 'r') as fp:
         color_name = row[0].replace('_', ' ')
         c_xyz = [float(x) for x in row[-3:]]
         d_xyz = [float(x) for x in row[-6: -3]]
+        j_rgb = [float(x) for x in row[-9: -6]]
+        print(j_rgb)
+        j_xyz = colour.sRGB_to_XYZ(j_rgb)
+        ppg_lab = ppg_data[color_name]['LAB']
+        ppg_xyz = colour.Lab_to_XYZ(ppg_lab, illuminant=colour.ILLUMINANTS['CIE 1931 2 Degree Standard Observer']['D65'])
 
-        xy = colour.XYZ_to_xy(c_xyz, illuminant=colour.ILLUMINANTS['CIE 1931 2 Degree Standard Observer']['D65'])
+        xy = colour.XYZ_to_xy(ppg_xyz, illuminant=colour.ILLUMINANTS['CIE 1931 2 Degree Standard Observer']['D65'])
         x, y = xy
-        plt.plot(x, y, 'o-', color='black')
+        plt.plot(x, y, 'o-', color='black', markersize=4)
 
         print(d_xyz, ' ', c_xyz)
 
--- Test.py
+++ Test.py
@@ -1,14 +1,77 @@
-import codecs
-import sys
-
-from PIL import Image
-import exiftool
 import numpy as np
+import rawpy as rp
+import matplotlib.pyplot as plt
 import json
+import Reflectance2XYZ as ref2xyz
 
-path = 'E:/UIUC/Data_10_07_18/no_flash/RAW_2018_10_07_11_05_37_707.dng'
-with exiftool.ExifTool() as et:
-    tmp = et.get_metadata(path)
-    for k in tmp.keys():
-        print(k, ':', tmp[k])
+ppg_data = ref2xyz.read_data('VOC 2014 Color Data.csv')
+
+path_im = 'E:/UIUC/Data_11_07_18/iOS'
+im_name = 'RAW_2018_11_08_15_09_06_866_noflash.dng'
+with open('E:/UIUC/Data_11_07_18/IOSNoFlashLabel.json', 'r') as fp:
+    labels = json.load(fp)
+
+key = 'HYDRANGEA FLORET'
+fig, (ax1, ax2, ax3, ax4, ax5) = plt.subplots(1, 5)
+with rp.imread(path_im + '/' + im_name) as raw:
+    patch_roi = labels[key][0]['patch_roi']
+
+    #Raw values
+    raw_values = np.array(raw.raw_image)
+    w, h = np.shape(raw_values)
+    rgb = np.zeros([int(w / 2), int(h / 2), 3], dtype=np.float)
+    cwb = raw.camera_whitebalance
+    print(cwb)
+    for i in range(0, np.shape(raw_values)[0], 2):
+        for j in range(0, np.shape(raw_values)[1], 2):
+            #print(raw.raw_color(i, j), ' ', raw.raw_color(i, j + 1), ' ', raw.raw_color(i + 1, j))
+            tmp = [raw_values[i][j], raw_values[i][j + 1], raw_values[i + 1][j + 1]]
+            rgb[int(i / 2)][int(j / 2)] = tmp
+    vmax = np.max(rgb)
+    print(vmax)
+    rgb = rgb / vmax
+    rgb = rgb * 255
+    rgb = rgb.clip(0, 255)
+    ax1.imshow(np.array(rgb[int(patch_roi[1]/2):int(patch_roi[3]/2), int(patch_roi[0]/2):int(patch_roi[2]/2)], np.int))
+    ax1.set_title('Raw image')
+    ax1.axis('off')
+
+    #white balance
+    raw_values = np.array(raw.raw_image)
+    w, h = np.shape(raw_values)
+    rgb = np.zeros([int(w / 2), int(h / 2), 3], dtype=np.float)
+    cwb = raw.camera_whitebalance
+    print(cwb)
+    for i in range(0, np.shape(raw_values)[0], 2):
+        for j in range(0, np.shape(raw_values)[1], 2):
+            # print(raw.raw_color(i, j), ' ', raw.raw_color(i, j + 1), ' ', raw.raw_color(i + 1, j))
+            tmp = [raw_values[i][j] - 528, raw_values[i][j + 1] - 528, raw_values[i + 1][j + 1] - 528]
+            rgb[int(i / 2)][int(j / 2)] = [tmp[0]*cwb[0], tmp[1]*cwb[1], tmp[2]*cwb[2]]
+    vmax = np.max(rgb)
+    print(vmax)
+    rgb = rgb / vmax
+    rgb = rgb * 255
+    rgb = rgb.clip(0, 255)
+    ax2.imshow(np.array(rgb[int(patch_roi[1]/2):int(patch_roi[3]/2), int(patch_roi[0]/2):int(patch_roi[2]/2)], np.int))
+    ax2.set_title('White balanced')
+    ax2.axis('off')
+
+    #color transform
+    rgb = raw.postprocess(use_camera_wb=True, gamma=(1, 1))
+    ax3.imshow(rgb[patch_roi[1]:patch_roi[3], patch_roi[0]:patch_roi[2]])
+    ax3.set_title('Color space transform')
+    ax3.axis('off')
+
+    #full
+    rgb = raw.postprocess(use_camera_wb=True)
+    ax4.imshow(rgb[patch_roi[1]:patch_roi[3], patch_roi[0]:patch_roi[2]])
+    ax4.set_title('Full pipeline')
+    ax4.axis('off')
+
+rgb = np.tile(ppg_data[key]['RGB'], [300, 300, 1])
+ax5.imshow(rgb)
+ax5.set_title('GT')
+ax5.axis('off')
+plt.show()
+
 
