ab2118e298312faa4bad7f31503d433420fab3eb
tuanbi97 <tuanbi97@gmail.com>
2018-12-10 09:26:46 -0600

    Check colorchecker

--- Test7.py
+++ Test7.py
@@ -71,13 +71,17 @@ cc = prepare_cc()
 path = 'E:/UIUC/Data_10_21_18/AndroidMacbeth/RAW_2018_10_21_15_32_27_912_noflash.dng'
 path = 'E:/UIUC/Data_11_07_18/Android/RAW_2018_11_09_02_34_34_360_noflash.dng'
 path = 'E:/UIUC/Data_11_10_18/android_dng/RAW_2018_11_11_06_33_46_930_noflash.dng'
+#Position of the first box
 x, y = (1100, 1174)
 box = [[], [], [], [], [], [],
        [], [], [], [], [], [],
        [], [], [], [], [], [],
        [], [], [], [], [], []]
+#Distance between 2 consecutive boxes
 offx = 360
 offy = 360
+
+#visualize
 path_jpeg = 'E:/UIUC/Data_11_10_18/android_jpg/JPEG_2018_11_11_06_33_46_930_noflash.jpg'
 rgb = cv2.cvtColor(cv2.imread(path_jpeg), cv2.COLOR_BGR2RGB)
 for i in range(0, 24):
@@ -86,22 +90,24 @@ for i in range(0, 24):
     rgb = cv2.rectangle(rgb, (box[i][0], box[i][1]), (box[i][2], box[i][3]), color=0, thickness=10)
 plt.imshow(rgb)
 plt.show()
+
+
 with rp.imread(path) as raw:
-    user_wb = cal_user_wb([1009, 2154, 1140, 2269], raw)
-    print(user_wb)
+    # user_wb = cal_user_wb([1009, 2154, 1140, 2269], raw)
+    # print(user_wb)
     print(raw.camera_whitebalance)
-    custom_xyz = raw.postprocess(user_wb = user_wb,
-                                #use_camera_wb = True,
+    im_xyz = raw.postprocess(#user_wb = user_wb,
+                                 use_camera_wb = True,
                                  output_bps=16,
                                  output_color=rp.ColorSpace.XYZ,
                                  gamma=(1, 1)).astype(float) / (2.0 ** 16 - 1)
-    rgb = raw.postprocess(user_wb = user_wb)
-    plt.imshow(rgb)
-    plt.show()
+    # rgb = raw.postprocess(user_wb = user_wb)
+    # plt.imshow(rgb)
+    # plt.show()
     fig, axs = plt.subplots()
     dEs = np.zeros((4, 6), dtype=np.float)
     for i in range(0, 24):
-        sample = custom_xyz[box[i][1]:box[i][3], box[i][0]:box[i][2]]
+        sample = im_xyz[box[i][1]:box[i][3], box[i][0]:box[i][2]]
         mean_xyz = [np.mean(sample[:, :, x]) for x in range(0, 3)]
         dE = colour.delta_E(colour.XYZ_to_Lab(mean_xyz, illuminant=colour.ILLUMINANTS['CIE 1931 2 Degree Standard Observer']['D65'])
                        , colour.XYZ_to_Lab(cc[i], illuminant=colour.ILLUMINANTS['CIE 1931 2 Degree Standard Observer']['D65']))
